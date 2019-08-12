29a66e583dbe9a941f69b28207b0f86de1f479fa
Allan Lotta <allanmatheus77@outlook.com>
2019-08-12 11:45:15 -0300

    added list providers and url to file works

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddlewere from './app/middleweres/auth';
 
@@ -20,4 +21,7 @@ routes.use(authMiddlewere);
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
+
 export default routes;
